9ca5dfaf099583079b1c0d12e76f7eb073c1cc25
wolpino <ariwolpin@gmail.com>
2026-06-22 14:42:01 -0700

    dev publish actions

---
+++ development/public_views.py
@@ -0,0 +1,65 @@
+from rest_framework.exceptions import NotFound
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import AllowAny
+
+from .models import DevelopmentRecipe
+from .serializers import PublicRecipeSerializer
+
+
+def build_public_recipe_payload(recipe: DevelopmentRecipe) -> dict:
+    if recipe.status != "published" or recipe.published_version_id is None:
+        raise NotFound("Recipe not found.")
+
+    version = recipe.published_version
+    fork_lineage = None
+    if recipe.user.show_forks and recipe.fork_record_id is not None:
+        source_recipe = recipe.fork_record.forked_from_version.recipe
+        fork_lineage = {
+            "title": source_recipe.title,
+            "author": recipe.fork_record.forked_from_user.username,
+            "slug": source_recipe.slug if source_recipe.status == "published" else None,
+        }
+
+    return {
+        "slug": recipe.slug,
+        "title": version.title,
+        "description": version.description,
+        "story": version.story,
+        "hero_image": version.hero_image,
+        "equipment_notes": version.equipment_notes,
+        "prep_minutes": version.prep_minutes,
+        "cook_minutes": version.cook_minutes,
+        "author": recipe.user.username,
+        "published_at": recipe.published_at,
+        "ingredient_lines": version.ingredient_lines.select_related("ingredient").all(),
+        "steps": version.steps.all(),
+        "fork_lineage": fork_lineage,
+    }
+
+
+class PublicRecipeView(RetrieveAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = PublicRecipeSerializer
+    lookup_field = "slug"
+
+    def get_object(self) -> dict:
+        recipe = (
+            DevelopmentRecipe.objects.filter(
+                slug=self.kwargs["slug"],
+                status="published",
+            )
+            .select_related(
+                "user",
+                "published_version",
+                "fork_record__forked_from_version__recipe",
+                "fork_record__forked_from_user",
+            )
+            .prefetch_related(
+                "published_version__ingredient_lines__ingredient",
+                "published_version__steps",
+            )
+            .first()
+        )
+        if recipe is None:
+            raise NotFound("Recipe not found.")
+        return build_public_recipe_payload(recipe)
